8fdd9e0b4a69bafbea65cf7c3fa7b3f20cce14c7
CedricA <cedric.j.aoun@gmail.com>
2025-07-07 14:02:40 +0200

    slide 17: Hello world added to index

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView contentInsetAdjustmentBehavior="automatic">
+      <View
+          style={{ 
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+        }}
+      >
+        <Text>Hello World from Cloud Campus student</Text>
+      </View>
+    </ScrollView>
   );
 }
