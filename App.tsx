53463ebe38143cb8d3984c2240ac800351728fd4
CedricA <cedric.j.aoun@gmail.com>
2025-07-07 16:09:57 +0200

    slide 41: add Tab and Drawer navigation

--- App.tsx
+++ App.tsx
@@ -1,33 +1,38 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 
 // Import your screens
-import { Button } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { Platform } from 'react-native';
 import Home from './components/Home';
+import News from './components/News';
 import Settings from './components/Settings';
 import { RootStackParamList } from './router';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<RootStackParamList>();
+const Drawer = createDrawerNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <>
       <StatusBar style="dark" backgroundColor="#44a" />
       <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="Home"
-        >
-          <Stack.Screen name="Home" component={Home} />
-          <Stack.Screen 
-            name="Settings" 
-            component={Settings} 
-            options={({ route }) => ({ headerRight: () => 
-              <Button onPress={() => {}} title="Save Changes" disabled={route.params.username === 'John Doe'} /> 
-            })} 
-          />
-        </Stack.Navigator>
+        {Platform.OS === 'ios' && (
+          <Tab.Navigator>
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="Settings" component={Settings} />
+            <Tab.Screen name="News" component={News} />
+          </Tab.Navigator>
+        )}
+        {Platform.OS === 'android' && (
+          <Drawer.Navigator>
+            <Drawer.Screen name="Home" component={Home} />
+            <Drawer.Screen name="Settings" component={Settings} />
+            <Drawer.Screen name="News" component={News} />
+          </Drawer.Navigator>
+        )}
       </NavigationContainer>
     </>
   );
