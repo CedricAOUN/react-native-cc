314fa0a2d402c67267bea35210b524f680566a56
CedricA <cedric.j.aoun@gmail.com>
2025-07-07 14:11:53 +0200

    slide 18: added mock audio player with stylesheets

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,39 @@
-import { ScrollView, Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
+  const name = "Best song ever";
+  const styles = StyleSheet.create({
+    container: {
+      margin: 10,
+      marginTop: 100,
+      backgroundColor: 'orange',
+      borderRadius: 5
+    },
+    innerContainer: {
+      backgroundColor: "brown",
+      height: 50,
+      width: 150,
+      borderTopLeftRadius: 5,
+      borderBottomLeftRadius: 5,
+    },
+    title: {
+      fontSize: 18,
+      fontWeight: '200',
+      color: 'white',
+      position: 'absolute',
+      top: 12,
+      left: 10,
+    },
+    subtitle: {
+      fontWeight: 'bold'
+    }
+  });
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <View
-          style={{ 
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-        }}
-      >
-        <Text>Hello World from Cloud Campus student</Text>
-      </View>
-    </ScrollView>
+    <View style={styles.container}>
+      <View style={styles.innerContainer} />
+      <Text style={styles.title}>
+        <Text style={styles.subtitle}>Playing: </Text>{name}
+      </Text>
+    </View>
   );
 }
