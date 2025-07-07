2c471f224e9f03bfa9fba28cefae4297dab2b399
CedricA <cedric.j.aoun@gmail.com>
2025-07-07 15:55:21 +0200

    slide 38: change navigation title with useEffect

--- components/Settings.tsx
+++ components/Settings.tsx
@@ -1,11 +1,18 @@
 import { RootStackParamList } from "@/router";
 import styles from "@/styles/styles";
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { useEffect } from "react";
 import { Button, Text, View } from "react-native";
 type Props = NativeStackScreenProps<RootStackParamList, 'Settings'>;
 
 export default function Settings({ navigation, route }: Props) {
   const { username } = route.params;
+  
+  useEffect(() => {
+    navigation.setOptions({
+      title: `Settings for ${username}`
+    });
+  }, []);
 
   return (
     <View style={styles.container}>
