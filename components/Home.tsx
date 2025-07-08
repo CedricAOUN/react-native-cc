d4aae313c9622e9b18b1b7e312e46611a6aee669
CedricA <cedric.j.aoun@gmail.com>
2025-07-08 16:55:13 +0200

    add toast to home page

--- components/Home.tsx
+++ components/Home.tsx
@@ -2,6 +2,8 @@ import { RootStackParamList } from "@/router";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { Text, View } from "react-native";
 import * as Progress from 'react-native-progress';
+import { RootSiblingParent } from "react-native-root-siblings";
+import Toast from 'react-native-root-toast';
 import styles from "../styles/styles";
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Home'>;
@@ -11,6 +13,11 @@ export default function Home({ navigation }: Props) {
     <View style={styles.container}>
       <Text>Welcome to the Home Screen</Text>
       <Progress.Circle progress={0.5} />
+      <RootSiblingParent>
+        <Text onPress={() => {
+          Toast.show("Hello World");
+        }}>Show Notification</Text>
+      </RootSiblingParent>
     </View>
   );
 }
